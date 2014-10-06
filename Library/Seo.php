134968e2423c27e7fa90aece3a4737ed16bd9cfb
Jarek <admin@boxbilling.com.pl>
2014-10-06 13:45:00 +0200

    Update Seo.php

--- Library/Seo.php
+++ Library/Seo.php
@@ -42,12 +42,37 @@ class Seo
 	
 	private static function __analitics($id)
 	{
-		
+		echo "<script>
+			  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+			
+			  ga('create', '{$id}', 'auto');
+			  ga('send', 'pageview');
+			
+			</script>";
 	}
 	
-	private static function __piwik($id)
+	###############################
+	#     $url_piwik - Bez http   #
+	#	  $id - id piwik id	      #
+	###############################
+	
+	private static function __piwik($id, $url_piwik)
 	{
-		
+		echo '<script type="text/javascript">
+			  var _paq = _paq || [];
+			  _paq.push([\'trackPageView\']);
+			  _paq.push([\'enableLinkTracking\']);
+			  (function() {
+				var u=(("https:" == document.location.protocol) ? "https" : "http") + "://'.$url_piwik.'";
+				_paq.push([\'setTrackerUrl\', u+\'piwik.php\']);
+				_paq.push([\'setSiteId\', '.$id.']);
+				var d=document, g=d.createElement(\'script\'), s=d.getElementsByTagName(\'script\')[0]; g.type=\'text/javascript\';
+				g.defer=true; g.async=true; g.src=u+\'piwik.js\'; s.parentNode.insertBefore(g,s);
+			  })();
+			</script>';
 	}
 	
 }
